e581b70ec940ca0031008494d135a9a2b83e6352
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2024-06-09 14:00:50 +0300

    Sonuç

--- AmazonUkPriceTracking/AmazonUkPriceTrackingController.cs
+++ AmazonUkPriceTracking/AmazonUkPriceTrackingController.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AmazonUkPriceTracking
@@ -7,12 +9,29 @@ namespace AmazonUkPriceTracking
     public class AmazonUkPriceTrackingController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetAttackonTitanCharacters()
         {
+            // From Web
             var url = "https://attackontitan.fandom.com/wiki/List_of_characters/Anime";
-            var client = new HttpClient();
-            var html = await client.GetStringAsync(url);
-            return Ok(html);
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            IList<HtmlNode> nodes = doc.QuerySelectorAll("div.characterbox-main")[1]
+                .QuerySelectorAll("div.characterbox-container table tbody");
+
+            var data = nodes.Select(node =>
+            {
+                var name = node.QuerySelector("tr:nth-child(2) th a").InnerText;
+                return new
+                {
+                    name = name,
+                    imageUrl = node.QuerySelector("tr td div a img")
+                        .GetAttributeValue("data-src",
+                            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F716487122224439296%2FHWPluyjs.jpg&f=1&nofb=1"),
+                    descriptionUrl = $"https://attackontitan.fandom.com/wiki/{name.Replace(" ", "_")}_(Anime)"
+                };
+            });
+            return Ok(data);
         }
     }
 }
